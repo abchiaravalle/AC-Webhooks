b8761991f083af317b946916aada116e9f80f82f
Adam Chiaravalle <adamchiaravalle@gmail.com>
2025-01-27 00:26:48 -0500

    v 1.1

--- ac-hooks.php
+++ ac-hooks.php
@@ -4,7 +4,7 @@
 /**
  * Plugin Name: Webhooks for CF7
  * Description: Add and manage Contact Form 7 forms and their webhook URLs in a WordPress admin panel.
- * Version: 1
+ * Version: 1.1
  * Author: Adam Chiaravalle @ ACWebDev, LLC.
  */
 
